2991131646bc61b4793159038151de283330a7e8
Jan Claes <jan.claes@xylos.com>
2022-10-02 21:02:32 +0200

    Workaround

--- main.tf
+++ main.tf
@@ -81,7 +81,7 @@ resource "azurerm_management_lock" "loganalyticsworkspacelock" {
 
 resource "azurerm_log_analytics_linked_service" "loganalyticsworkspacelinkedservice" {
   resource_group_name = data.azurerm_resource_group.terraform.name
-  workspace_id        = var.lawid
+  workspace_id        = replace(replace(azurerm_log_analytics_workspace.loganalyticsworkspace.id, "microsoft.operationalinsights", "Microsoft.OperationalInsights"), "resourcegroups", "resourceGroups")
   read_access_id      = azurerm_automation_account.automationaccount.id
 }
 
