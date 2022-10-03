4a7b6cf9ac89ae43114ef5f60d89a45be17bdd9a
Jan Claes <jan.claes@xylos.com>
2022-10-03 20:13:13 +0200

    Update outputs to show

--- outputs.tf
+++ outputs.tf
@@ -10,26 +10,6 @@ output "law-name" {
   value = azurerm_log_analytics_workspace.loganalyticsworkspace.name
 }
 
-output "vault-name" {
-  value = azurerm_recovery_services_vault.keyvault.name
-}
-
 output "aa-name" {
   value = azurerm_automation_account.automationaccount.name
-}
-
-output "rg-name" {
-  value = azurerm_resource_group.terraform.name
-}
-
-output "rg-id" {
-  value = azurerm_resource_group.terraform.id
-}
-
-output "diagblob_endpoint" {
-  value = azurerm_storage_account.bootdiags.primary_blob_endpoint
-}
-
-output "diagnostics-sa-id" {
-  value = azurerm_storage_account.gendiags.id
 }
