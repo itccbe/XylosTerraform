8f07187c5adbec0a11f5adf14ecdb5cf943907ab
Jan Claes <jan.claes@xylos.com>
2022-10-07 11:59:37 +0200

    test push

--- main.tf
+++ main.tf
@@ -81,7 +81,7 @@ resource "azurerm_management_lock" "loganalyticsworkspacelock" {
 
 resource "azurerm_log_analytics_linked_service" "loganalyticsworkspacelinkedservice" {
   resource_group_name = data.azurerm_resource_group.terraform.name
-  workspace_id        = var.lawid
+  workspace_id        = var.lawidc
   read_access_id      = azurerm_automation_account.automationaccount.id
 }
 
