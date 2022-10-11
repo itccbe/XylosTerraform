ffd63d5eec7923f7b2757744e5e94fd489022881
Jan Claes <jan.claes@xylos.com>
2022-10-11 21:08:21 +0200

    Add keyvault

--- main.tf
+++ main.tf
@@ -24,6 +24,35 @@ resource "azurerm_storage_share" "azureshare" {
   storage_account_name = azurerm_storage_account.storageaccount.name
 }
 
+resource "azurerm_key_vault" "keyvault" {
+  name                        = "xyloskeyvault"
+  location                    = data.azurerm_resource_group.terraform.location
+  resource_group_name         = data.azurerm_resource_group.terraform.name
+  enabled_for_disk_encryption = true
+  tenant_id                   = data.azurerm_client_config.current.tenant_id
+  soft_delete_retention_days  = 7
+  purge_protection_enabled    = false
+
+  sku_name = "standard"
+
+  access_policy {
+    tenant_id = data.azurerm_client_config.current.tenant_id
+    object_id = data.azurerm_client_config.current.object_id
+
+    key_permissions = [
+      "Get",
+    ]
+
+    secret_permissions = [
+      "Get",
+    ]
+
+    storage_permissions = [
+      "Get",
+    ]
+  }
+}
+
 # resource "azurerm_automation_account" "automationaccount" {
 #   name                = "itccautomation001"
 #   location            = data.azurerm_resource_group.terraform.location
