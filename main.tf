bd509b0fbd961a20c0fbca1b153a75aa086ae1cb
Jan Claes <jan.claes@xylos.com>
2022-10-11 21:59:49 +0200

    Add VM for cost calculation

--- main.tf
+++ main.tf
@@ -53,6 +53,63 @@ resource "azurerm_key_vault" "keyvault" {
   }
 }
 
+resource "azurerm_virtual_network" "virtualnetwork" {
+  name                = "xylosvnet001"
+  location            = data.azurerm_resource_group.terraform.location
+  resource_group_name = data.azurerm_resource_group.terraform.name
+  address_space       = ["10.0.0.0/16"]
+  subnet {
+    name           = "subnet1"
+    address_prefix = "10.0.1.0/24"
+  }
+}
+
+resource "random_password" "sftp" {
+  length           = 16
+  special          = true
+  override_special = "!#$%&*()-_=+[]{}<>:?"
+}
+
+resource "azurerm_network_interface" "sftp" {
+  name                = "${var.customer_abbreviation}-${var.environment}-sftp-nic"
+  location            = data.azurerm_resource_group.terraform.location
+  resource_group_name = data.azurerm_resource_group.terraform.name
+
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = data.azurerm_subnet.subnet1
+    private_ip_address_allocation = "Dynamic"
+  }
+}
+
+resource "azurerm_linux_virtual_machine" "sftp" {
+  name                = "${var.customer_abbreviation}-${var.environment}-sftp-vm"
+  location            = data.azurerm_resource_group.terraform.location
+  resource_group_name = data.azurerm_resource_group.terraform.name
+  size                = "Standard_D2s_v5"
+  admin_username      = var.environment
+  admin_password = random_password.sftp.result
+  disable_password_authentication = false
+
+  network_interface_ids = [
+    azurerm_network_interface.sftp.id,
+  ]
+
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Premium_LRS"
+    disk_size_gb = 128
+    name = "${var.customer_abbreviation}-${var.environment}-sftp-osdisk"
+  }
+
+  source_image_reference {
+    publisher = "Canonical"
+    offer     = "0001-com-ubuntu-server-focal"
+    sku       = "20_04-lts"
+    version   = "latest"
+  }
+}
+
 # resource "azurerm_automation_account" "automationaccount" {
 #   name                = "itccautomation001"
 #   location            = data.azurerm_resource_group.terraform.location
