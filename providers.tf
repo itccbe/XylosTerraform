7ba910386377e6211a4e5037f97bc5a3416dedec
Jan Claes <jan.claes@xylos.com>
2022-10-24 20:09:07 +0200

    Update SA for ITCCBE

--- providers.tf
+++ providers.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "azurerm" {
     resource_group_name  = "terraform-statefile"
-    storage_account_name = "statefilestorage002"
+    storage_account_name = "statefilestorage001"
     container_name       = "statefile"
     key                  = "terraform.tfstate"
   }
