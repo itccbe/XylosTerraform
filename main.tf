45b83096b317d196fbbe8e560830b7f2302a4d90
Jan Claes <jan.claes@xylos.com>
2022-10-27 11:32:51 +0200

    Add new VNet and subnet

--- main.tf
+++ main.tf
@@ -53,16 +53,16 @@ resource "azurerm_key_vault" "keyvault" {
   }
 }
 
-# resource "azurerm_virtual_network" "virtualnetwork" {
-#   name                = "xylosvnet001"
-#   location            = data.azurerm_resource_group.terraform.location
-#   resource_group_name = data.azurerm_resource_group.terraform.name
-#   address_space       = ["10.0.0.0/16"]
-#   subnet {
-#     name           = "subnet1"
-#     address_prefix = "10.0.1.0/24"
-#   }
-# }
+resource "azurerm_virtual_network" "virtualnetwork" {
+  name                = "xylosvnet001"
+  location            = var.location
+  resource_group_name = var.resource_group_name
+  address_space       = ["10.0.0.0/16"]
+  subnet {
+    name           = "subnet1"
+    address_prefix = "10.0.1.0/24"
+  }
+}
 
 # resource "random_password" "sftp" {
 #   length           = 16
