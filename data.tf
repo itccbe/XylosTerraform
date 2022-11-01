5399bda6d19df76240c2165b2abcf71fb4f7c7ef
Jan Claes <jan.claes@xylos.com>
2022-11-01 22:20:58 +0100

    Remove subnet block in data.tf

--- data.tf
+++ data.tf
@@ -1,7 +1,7 @@
-data "azurerm_subnet" "subnet1" {
-  name                 = "subnet1"
-  virtual_network_name = azurerm_virtual_network.virtualnetwork.name
-  resource_group_name  = var.resource_group_name
-}
+# data "azurerm_subnet" "subnet1" {
+#   name                 = "subnet1"
+#   virtual_network_name = azurerm_virtual_network.virtualnetwork.name
+#   resource_group_name  = var.resource_group_name
+# }
 
 data "azurerm_client_config" "current" {}
